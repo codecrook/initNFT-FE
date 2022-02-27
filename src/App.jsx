dd1d9b3ec6ce1bdef6ad1ad5915a07e8eafbcbb4
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2022-02-28 01:38:37 +0530

    ADD: show popup with OpenSea link when NFT is minted

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,32 @@ import './App.css';
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+  const CONTRACT_ADDRESS = "CA";
+
+  // function to setup event listener.
+  const setupEventListener = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+        });
+
+        console.log("Setup event listener!")
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  };
 
   // function to check if MetaMask connected
   const checkIfWalletIsConnected = async () => {
@@ -25,6 +51,8 @@ const App = () => {
       const [account] = accounts;
       console.log('Found Authorized Account:', account);
       setCurrentAccount(account);
+
+      setupEventListener();
     } else {
       console.log('Authorized Account not Found!');
     }
@@ -47,8 +75,6 @@ const App = () => {
 
   // function to mint the NFT
   const askContractToMintNft = async () => {
-    const CONTRACT_ADDRESS = "CA";
-
     try {
       const { ethereum } = window;
 
@@ -71,7 +97,7 @@ const App = () => {
     } catch (error) {
       console.log(error)
     }
-  }
+  };
 
   // Render Methods
   const renderNotConnectedContainer = () => (
